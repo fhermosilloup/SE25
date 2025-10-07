289ea2efd6212a8de75a4f532183b265349634b9
fhermosilloup <fhermosillo@up.edu.mx>
2025-10-07 10:38:51 -0600

    Update test.c

--- session/kernel/example_01/test.c
+++ session/kernel/example_01/test.c
@@ -6,22 +6,23 @@
 /* Función que se ejecuta cuando se instala el driver con insmod */
 static int __init my_driver_init(void)
 {
-	printk(KERN_INFO "Hello world from kernel space!");
-	return 0;
+        printk(KERN_INFO "Hello world from kernel space!");
+        return 0;
 }
 
 /* Función que se ejecuta cuando se desinstala el driver con rmmod */
 static void __exit my_driver_exit(void)
 {
-	printk(KERN_INFO "Godbye from kernel space\n");
+        printk(KERN_INFO "Godbye from kernel space\n");
 }
 
 /* Registro de ambas funciones */
-module_init(my_kernel_module_init);
-module_exit(my_kernel_module_exit);
+module_init(my_driver_init);
+module_exit(my_driver_exit);
 
 /* Descripción del driver */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Universidad Panamericana");
 MODULE_DESCRIPTION("The hello world of a Linux kernel module");
 MODULE_VERSION("1.0");
+
