5e85648ad17ddd2a0a9d3cbad8ecc94b6541d843
fhermosilloup <fhermosillo@up.edu.mx>
2025-10-21 16:22:56 -0600

    Create main.c

---
+++ session/kernel/example_gpio_basic/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#define LED_ON  _IO('a',1)
+#define LED_OFF _IO('a',2)
+#define BUTTON_READ _IOR('a',3,int)
+
+int main() {
+    int fd = open("/dev/led_button_ioctl", O_RDWR);
+    int val;
+
+    ioctl(fd, LED_ON);
+    sleep(1);
+    ioctl(fd, LED_OFF);
+
+    ioctl(fd, BUTTON_READ, &val);
+    printf("Button value: %d\n", val);
+
+    close(fd);
+    return 0;
+}
